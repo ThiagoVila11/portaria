4504bd8d185dc85438c150538759dab1ebf85919
ThiagoVila11 <simbiox.thiago.tosatti@vila11.com.br>
2025-09-25 16:22:25 -0300

    acertos

--- core/params.py
+++ core/params.py
@@ -1,14 +1,35 @@
 # core/params.py
-from django.core.cache import cache
-from portaria.models import Parametro
+#from django.core.cache import cache
+#from portaria.models import Parametro
 
-def get_param(name: str, default=None, ttl=60):
-    key = f"param:{name}"
-    val = cache.get(key)
-    if val is None:
-        val = (Parametro.objects
-               .filter(ParametroNome=name)
-               .values_list("ParametroValor", flat=True)
-               .first())
-        cache.set(key, val, ttl)
-    return val if val is not None else default
+#def get_param(name: str, default=None, ttl=60):
+#    key = f"param:{name}"
+#    val = cache.get(key)
+#    if val is None:
+#        val = (Parametro.objects
+#               .filter(ParametroNome=name)
+#               .values_list("ParametroValor", flat=True)
+#               .first())
+#        cache.set(key, val, ttl)
+#    return val if val is not None else default
+
+from django.db.utils import OperationalError, ProgrammingError
+
+
+def get_param(key, default=None):
+    """
+    Busca um parâmetro da tabela Parametro.
+    Se o banco ainda não estiver pronto (sem tabela/migrações), retorna o default.
+    """
+    try:
+        from portaria.models import Parametro
+        val = (
+            Parametro.objects
+            .filter(chave=key)
+            .values_list("valor", flat=True)
+            .first()
+        )
+        return val if val is not None else default
+    except (OperationalError, ProgrammingError):
+        # Banco não disponível ou tabela não criada ainda
+        return default
--- portaria/apps.py
+++ portaria/apps.py
@@ -1,8 +1,27 @@
+#from django.apps import AppConfig
+
+#class PortariaConfig(AppConfig):
+#    default_auto_field = "django.db.models.BigAutoField"
+#    name = "portaria"
+
+#    def ready(self):
+#        import portaria.signals  # noqa
+
 from django.apps import AppConfig
+from django.db.utils import OperationalError, ProgrammingError
+
 
 class PortariaConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "portaria"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'portaria'
 
     def ready(self):
-        import portaria.signals  # noqa
+        """
+        Carrega os signals apenas quando o banco já estiver utilizável.
+        Evita erro em `python manage.py migrate` inicial.
+        """
+        try:
+            import portaria.signals  # noqa
+        except (OperationalError, ProgrammingError):
+            # Evita quebrar durante migrações iniciais
+            pass
