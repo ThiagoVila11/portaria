61b166243a421e39281cb99e375701190cd87585
ThiagoVila11 <simbiox.thiago.tosatti@vila11.com.br>
2025-09-26 15:22:33 -0300

    settings

--- condominio_portaria/settings.py
+++ condominio_portaria/settings.py
@@ -76,7 +76,8 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://10.1.10.86:8000",
+    #"http://10.1.10.86:8000",
+    "http://10.0.10.10:8000",
 ]
 
 CSRF_COOKIE_SECURE = False
